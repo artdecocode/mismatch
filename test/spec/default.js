000711b2033b044ca86fd2f28b1fb26d5f15ef4d
Anton <anton@adc.sh>
2018-07-21 04:45:46 +0100

    add tests

--- test/spec/default.js
+++ test/spec/default.js
@@ -1,4 +1,4 @@
-import { equal, ok } from 'zoroaster/assert'
+import { equal, deepEqual } from 'zoroaster/assert'
 import Context from '../context'
 import mismatch from '../../src'
 
@@ -8,14 +8,75 @@ const T = {
   'is a function'() {
     equal(typeof mismatch, 'function')
   },
-  async 'calls package without error'() {
-    await mismatch()
+  'finds no matches'() {
+    const re = /simple re/
+    const s = 'test'
+    const keys = ['t', 's']
+    const res = mismatch(re, s, keys)
+    deepEqual(res, [])
   },
-  async 'gets a link to the fixture'({ FIXTURE }) {
-    const res = await mismatch({
-      type: FIXTURE,
-    })
-    ok(res, FIXTURE)
+  'finds a single match'() {
+    const re = /(test) inside of a (string)/i
+    const s = 'Test inside of a STRING.'
+    const keys = ['test', 'string']
+    const res = mismatch(re, s, keys)
+    deepEqual(res, [
+      {
+        test: 'Test',
+        string: 'STRING',
+      },
+    ])
+  },
+  'does not add undefined key'() {
+    const re = /(test) inside of a (string)(\d)/i
+    const s = 'Test inside of a STRING1.'
+    const keys = ['test']
+    const res = mismatch(re, s, keys)
+    deepEqual(res, [
+      {
+        test: 'Test',
+      },
+    ])
+  },
+  'does not add undefined value'() {
+    const re = /(test) inside of a (string)(\d)?/i
+    const s = 'Test inside of a STRING.'
+    const keys = ['test', 'string', 'number']
+    const res = mismatch(re, s, keys)
+    deepEqual(res, [
+      {
+        test: 'Test',
+        string: 'STRING',
+      },
+    ])
+  },
+  'ignores missing keys'() {
+    const re = /(test)/i
+    const s = 'Test inside of a STRING1.'
+    const keys = ['test', 'hello', 'world']
+    const res = mismatch(re, s, keys)
+    deepEqual(res, [
+      {
+        test: 'Test',
+      },
+    ])
+  },
+  'finds multiple matches'() {
+    const re = /(test) inside of a (string)(\d)?/ig
+    const s = 'Test inside of a STRING1. TEST inside of a string.'
+    const keys = ['test', 'string', 'number']
+    const res = mismatch(re, s, keys)
+    deepEqual(res, [
+      {
+        test: 'Test',
+        string: 'STRING',
+        number: '1',
+      },
+      {
+        test: 'TEST',
+        string: 'string',
+      },
+    ])
   },
 }
 
