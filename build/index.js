69a54636f602552c330968f660ccb9b5bb30afac
Anton <anton@adc.sh>
2018-07-21 05:05:32 +0100

    build

--- build/index.js
+++ build/index.js
@@ -3,26 +3,50 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = mismatch;
+exports.default = void 0;
 
-var _util = require("util");
-
-const LOG = (0, _util.debuglog)('mismatch');
 /**
- * A JavaScript package to return captured groups of a regular expression as an object.
- * @param {Config} config Configuration object.
- * @param {string} config.type The type.
+ * Returns an array of captured groups of a regular expression as objects. Does not modify the `lastIndex` property of the regex.
+ * @param {RegExp} re The regular expression used for matching.
+ * @param {string} string String to find matches in.
+ * @param {string[]} keys Keys to use to create an captured group object.
+ * @example
+ *
+ * const re = /(test) inside of a (string)(\d)?/ig
+ * const s = 'Test inside of a STRING1. TEST inside of a string.'
+ * const keys = ['test', 'string', 'number']
+ * const res = mismatch(re, s, keys)
+ *
+ * // result:
+ * [
+ *  {
+ *    test: 'Test',
+ *    string: 'STRING',
+ *    number: '1',
+ *  },
+ *  {
+ *    test: 'TEST',
+ *    string: 'string',
+ *  },
+ * ]
  */
+function mismatch(re, string, keys) {
+  /** @type {Object.<string, string>[]} */
+  const m = [];
+  string.replace(re, (match, ...args) => {
+    const p = args.slice(0, args.length - 2); // remove position and input
 
-async function mismatch(config = {}) {
-  const {
-    type
-  } = config;
-  LOG('mismatch called with %s', type);
-  return type;
+    const o = p.reduce((acc, capturedGroup, i) => {
+      const key = keys[i];
+      if (!(key && capturedGroup)) return acc;
+      acc[key] = capturedGroup;
+      return acc;
+    }, {});
+    m.push(o);
+  });
+  return m;
 }
-/**
- * @typedef {Object} Config
- * @property {string} type The type.
- */
+
+var _default = mismatch;
+exports.default = _default;
 //# sourceMappingURL=index.js.map
