5ec1be083506acc1ce26d63b5dc10d70884e6d2f
Anton <anton@adc.sh>
2019-04-08 19:01:47 +0300

    update types for closure

--- build/index.js
+++ build/index.js
@@ -2,7 +2,7 @@
  * Returns an array of captured groups of a regular expression as objects. Does not modify the `lastIndex` property of the regex.
  * @param {RegExp} re The regular expression used for matching.
  * @param {string} string String to find matches in.
- * @param {string[]} keys Keys to use to create an captured group object.
+ * @param {Array<string>} keys Keys to use to create an captured group object.
  * @example
  *
  * const re = /(test) inside of a (string)(\d)?/ig
@@ -24,7 +24,7 @@
  * ]
  */
                function mismatch(re, string, keys) {
-  /** @type {Object.<string, string>[]} */
+  /** @type {Array<Object<string, string>>} */
   const m = []
   string.replace(re, (match, ...args) => {
     const p = args.slice(0, args.length - 2) // remove position and input
